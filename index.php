3a2d07eb6395ef488f42ae02f0995cb35f2daf49
phuocloc98 <phuocloc17598@gmail.com>
2020-05-31 17:11:11 +0700

    add new file

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+
+	<title>Haha cái gì mà haha, thì sao!</title>
+
+
+</head>
+<body>
+	<label>Hỏi mà ai cũng im thế nhỉ? Hello mấy bạn!</label>
+</body>
+</html>
